e1b9e0facfa7ce451b8121f45b0ec88f9f12f9a3
dmnted66 <dementedpccian@gmail.com>
2017-03-21 20:55:51 +0800

    Create ProjectDemo.java

---
+++ SetB/VolumeConverter/src/resources/ProjectDemo.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)ProjectDemo.java
+ *
+ * Bryle John Ballon, Jasper Fatalla
+ *
+ * BSCS-IIB 
+ * @version 1.00 2017/3/5
+ */
+
+
+public class ProjectDemo extends MainForm
+{
+
+	public static void main(String[] args)
+	{
+		MainForm myForm = new MainForm();
+		myForm.setVisible(true);
+	}
+}
